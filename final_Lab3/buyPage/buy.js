8a9949e7076ee049bc6d6f7598e6d2959c55736d
milimur <121692946+milimur@users.noreply.github.com>
2024-06-27 16:50:55 -0300

    Add files via upload

--- final_Lab3/buyPage/buy.js
+++ final_Lab3/buyPage/buy.js
@@ -1,85 +1,33 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Event listener para el botón de "Quitar" en el carrito de compras
-    document.querySelectorAll('.btn.text-danger').forEach(button => {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            this.closest('.row').remove();
-            updateCartTotal();
-        });
-    });
+window.onload = function() {
+    const accountLink = document.getElementById('account-link');
+    const btncompra = document.getElementById('btncompra');
+    var toastElement = new bootstrap.Toast(document.getElementById('loginToast'));
 
-    // Event listener para la actualización de la cantidad de productos
-    document.querySelectorAll('.form-control[type="number"]').forEach(input => {
-        input.addEventListener('change', function() {
-            updateCartTotal();
-        });
-    });
+    const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+        const [name, value] = cookie.split('=');
+        acc[name] = value;
+        return acc;
+    }, {});
 
-    // Actualizar el total del carrito
-    function updateCartTotal() {
-        let total = 0;
-        document.querySelectorAll('.row.gy-3.mb-4').forEach(row => {
-            const priceElement = row.querySelector('.h6').innerText.replace('$', '');
-            const quantityElement = row.querySelector('.form-control[type="number"]').value;
-            const price = parseFloat(priceElement);
-            const quantity = parseInt(quantityElement);
-            total += price * quantity;
-        });
-        document.querySelector('.card-body .fw-bold').innerText = `$${total}`;
+    if (cookies.email) {
+        accountLink.href = "/userPage/profile/Index.html";
+    } else {
+        accountLink.href = "/userPage/logIn/Index.html";
+        btncompra.disabled = false;
     }
 
-    // Event listener formulario de búsqueda
-    document.querySelector('form[role="search"]').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const query = this.querySelector('input[type="search"]').value;
-        window.location.href = `/productPage/filter.html?search=${query}`;
-    });
-
-    // Event listener botón de "Aplicar" cupón
-    document.querySelector('.btn.btn-light.border').addEventListener('click', function(event) {
-        event.preventDefault();
-        // Aplicar el cupón
-        alert('Cupón aplicado!');
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('shipping-form');
-    const modal = new bootstrap.Modal(document.getElementById('confirmacionModal'));
-
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        modal.show();
-    });
-});
-
-        // Agrega tu public key
-        const mp = new MercadoPago('YOUR_PUBLIC_KEY', {
-            locale: 'es-AR'
-        });
-
-        // Manejar la selección del método de pago
-        function showForm(formId) {
-            document.querySelectorAll('.payment-form').forEach(function(form) {
-                form.style.display = 'none';
-            });
-            document.querySelectorAll('.payment-option').forEach(function(option) {
-                option.classList.remove('active');
-            });
-            document.getElementById(formId).style.display = 'block';
-            document.getElementById('option' + formId.split('-')[1]).classList.add('active');
+    btncompra.addEventListener('click', function(event) {
+        if (!cookies.email) {
+            event.preventDefault();
+            toastElement.show();
+        } else {
+            window.location.href = "/buyPage/shippingPage.html";
         }
+    });
+};
 
-        // Crear el botón de pago
-        const checkoutButton = mp.checkout({
-            preference: {
-                id: 'YOUR_PREFERENCE_ID' // Reemplaza con el ID de tu preferencia de pago
-            },
-            render: {
-                container: '#form-mercado-pago', // Indica el lugar donde se renderizará el botón
-                label: 'Pagar con Mercado Pago' // Cambia el texto del botón si es necesario
-            }
-        });
-   
+function redirectToFilterPage() {
+    window.location.href = '/filterProductPage/filter.html';
+}
+
+
